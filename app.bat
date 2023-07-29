e4b8b1992765069260b6e59f85ec441beb541d92
Ceyhun Eren <ceyeren@gmail.com>
2023-07-30 01:20:54 +0300

    düzenleme

--- app.bat
+++
@@ -1,1 +0,0 @@
-start cmd.exe /k "node "C:/Users/ceyer/AppData/Roaming/npm/node_modules/appium/build/lib/main.js" -a 127.0.0.1 -p 4723 &"
--- app.cmd
+++ app.cmd
@@ -1,1 +1,3 @@
 start cmd.exe /k "node "C:/Users/ceyer/AppData/Roaming/npm/node_modules/appium/build/lib/main.js" -a 127.0.0.1 -p 4723 &"
+start cmd.exe /k "taskkill /F /IM node.exe"
+start cmd.exe /k "taskkill /F /IM cmd.exe"
